e53013c8721086e096af1e059a7b7a35c387fcca
Naser Abdullah <Naser507@example.com>
2026-03-10 07:56:25 +0600

    Initial setup test 1.2

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -9,6 +9,6 @@ read -p "Enter commit message: " msg
 git commit -m "$msg"
 
 echo "Pushing to origin..."
-git push origin main  # or 'master' if your default branch is master
+git push origin master  # or 'master' if your default branch is master
 
 echo "Update complete!"
